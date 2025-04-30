6b441c4839220558bd484eb52b27a52d793d800d
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-04-30 16:40:48 +0400

    Update address format

--- cmd/livefuzzer/main.go
+++ cmd/livefuzzer/main.go
@@ -62,7 +62,7 @@ func initApp() *cli.App {
 var app = initApp()
 
 func main() {
-	// zond.sendTransaction({from:personal.listAccounts[0], to:"0xb02A2EdA1b317FBd16760128836B0Ac59B560e9D", value: "100000000000000"})
+	// zond.sendTransaction({from:personal.listAccounts[0], to:"Zb02A2EdA1b317FBd16760128836B0Ac59B560e9D", value: "100000000000000"})
 	if err := app.Run(os.Args); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
